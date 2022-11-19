ccd8514caf87f283521671ad139a3c58f4ffa276
Apricot Falcon <cjhelms1428@gmail.com>
2022-11-19 21:59:46 +0000

    remove period

--- init.lua
+++ init.lua
@@ -72,7 +72,7 @@ local on_attach = function(client, bufnr)
   -- enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- mappings.
+  -- mappings
   -- see `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
