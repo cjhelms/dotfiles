7d2fd8ef7d2eafaaa7c091483875e29d0b5ecbfe
Chris <cjhelms1428@gmail.com>
2022-11-21 21:13:27 -0600

    fix cmd keybinds

--- init.lua
+++ init.lua
@@ -101,29 +101,54 @@ cmp.setup({
     completion=cmp.config.window.bordered(),
     documentation=cmp.config.window.bordered(),
   },
-  mapping={
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+  completion={
+    completeopt = 'menu,menuone,noinsert'
   },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete()),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-n>'] = {
+        c = function(fallback)
+            local cmp = require('cmp')
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+    },
+    ['<C-p>'] = {
+        c = function(fallback)
+            local cmp = require('cmp')
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+    },
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        else
+            fallback()
+        end
+    end,
+    ['<S-Tab>'] = function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end,
+  }),
   sources=cmp.config.sources({
     { name='nvim_lsp' },
     { name='luasnip' },
