bea02f460de3aa9a1281294ede673ad13b15d4a8
chris <cjhelms1428@gmail.com>
2023-08-23 00:47:26 -0500

    Teaks to install-minimal

--- install-minimal.sh
+++ install-minimal.sh
@@ -1,15 +1,22 @@
 # Miscellaneous tools
-sudo apt install wget git fzf stow xclip npm -y
+sudo apt install curl unzip wget git fzf stow xclip npm -y
 
-# Install neovim
+# Ensure Python pip is installed
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+rm get-pip.py
+
+# TODO: Ensure Python venv is installed
+
+# Install npm (necessary for Pyright language server)
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install nodejs
+
+# Install Neovim
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar -xvf nvim-linux64.tar.gz
-rm install nvim-linux64.tar.gz
-if [ ! -d "~/opt" ]; then
-  mkdir ~/opt
-fi
-mv nvim-linux64 ../opt
-if [ ! -d "~/bin" ]; then
-  mkdir ~/bin
-fi
+rm nvim-linux64.tar.gz
+mkdir -p ~/opt
+mv -f nvim-linux64 ~/opt
+mkdir -p ~/bin
 ln -s ~/opt/nvim-linux64/bin/nvim ~/bin/nvim
