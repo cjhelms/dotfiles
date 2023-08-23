c6c1093833661f581973d7412230040016d206b3
chris <cjhelms1428@gmail.com>
2023-08-22 23:55:34 -0500

    Update neovim download link

--- install-neovim.sh
+++ install-neovim.sh
@@ -2,6 +2,6 @@
 sudo apt install git fzf stow xclip npm -y
 
 # Install neovim
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 sudo apt install nvim-linux64.deb
 rm install nvim-linux64.deb
