122b3dbf41364a68f7d16d75d687c811f1899382
chris <cjhelms1428@gmail.com>
2023-08-23 00:11:29 -0500

    Add ignore file

--- install-neovim.sh
+++ install-neovim.sh
@@ -3,5 +3,13 @@ sudo apt install git fzf stow xclip npm -y
 
 # Install neovim
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-sudo apt install nvim-linux64.deb
-rm install nvim-linux64.deb
+tar -xvf nvim-linux64.tar.gz
+rm install nvim-linux64.tar.gz
+if [ ! -d "~/opt" ]; then
+  mkdir ~/opt
+fi
+mv nvim-linux64 ../opt
+if [ ! -d "~/bin" ]; then
+  mkdir ~/bin
+fi
+ln -s ~/opt/nvim-linux64/bin/nvim ~/bin/nvim
