c84bcdce26a1b36f6650c6bb8f0f842ea3708057
Chris Helms <cjhelms1428@gmail.com>
2023-08-30 19:17:06 -0500

    Make tokyonight transparent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("user.plugins")
+require("user.tokyonight")
 require("user.settings")
 require("user.lualine")
 require("user.comment")
