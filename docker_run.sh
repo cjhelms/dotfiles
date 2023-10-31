a88f4a2f344022a92173e8e91acd9a43a6e09153
Chris Helms <cjhelms1428@gmail.com>
2023-10-30 20:45:13 -0500

    Update docker run files

---
+++ docker_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+IMAGE_NAME="$1"
+IMAGE_TAG="latest"
+if [ -n "${2}" ]; then
+	IMAGE_TAG="${2}"
+fi
+SCRIPT_PATH=$(readlink "$0")
+HOST_VOLUME_PATH=$(dirname "${SCRIPT_PATH}")
+docker run \
+	--name "${IMAGE_NAME}" \
+	--gpus all \
+	-it \
+	--rm \
+	-v "${HOST_VOLUME_PATH}":/root/prj \
+	-v /home/chris/.gitconfig:/root/.gitconfig \
+	-v /home/chris/.ssh:/root/.ssh \
+	"${IMAGE_NAME}":"${IMAGE_TAG}" \
+	bash
