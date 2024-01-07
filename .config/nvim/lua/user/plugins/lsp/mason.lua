8b6b9a93507e21e9ce4738b7cf3f1b7429b96de6
Chris Helms <cjhelms1428@gmail.com>
2024-01-06 20:05:51 -0600

    nvim-lspconfig to lazy

--- .config/nvim/lua/user/plugins/lsp/mason.lua
+++ .config/nvim/lua/user/plugins/lsp/mason.lua
@@ -1,14 +1,4 @@
-local function configure()
-  require("mason").setup()
-  require("mason-lspconfig").setup({
-    automatic_installation = true,
-  })
-end
-
 return {
   "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim"
-  },
-  config = configure
+  config = function() require("mason").setup() end
 }
---
+++ .config/nvim/lua/user/plugins/lsp/nvim-lspconfig.lua
@@ -0,0 +1,75 @@
+local on_attach = function(_, bufnr)
+  local map = function(keybind, action, description)
+    if description then description = "LSP: " .. description end
+    vim.keymap.set("n", keybind, action, { buffer = bufnr, desc = description })
+  end
+
+  map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+  map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+  map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  map("K", vim.lsp.buf.hover, "Hover Documentation")
+  map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+  map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  map("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  map(
+    "<leader>ws",
+    require("telescope.builtin").lsp_dynamic_workspace_symbols,
+    "[W]orkspace [S]ymbols"
+  )
+  map(
+    "<leader>wl",
+    function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+    "[W]orkspace [L]ist Folders"
+  )
+
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = "rounded",
+        source = "always",
+        prefix = " ",
+        scope = "cursor",
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end,
+  })
+end
+
+local function setup_servers()
+  lspconfig = require("lspconfig")
+  -- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+  local function setup(name)
+    lspconfig[name].setup({
+      on_attach = on_attach,
+    })
+  end
+
+  setup("pyright")
+end
+
+local function configure()
+  require("mason-lspconfig").setup({
+    automatic_installation = true,
+  })
+  setup_servers()
+end
+
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  --   "hrsh7th/cmp-nvim-lsp",
+  },
+  config = configure
+}
