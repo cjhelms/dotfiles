76bc53930a69aca97944c078de421868e1694d2c
chris <cjhelms1428@gmail.com>
2024-01-06 22:55:30 -0600

    Clean up nvim-cmp

--- .config/nvim/lua/user/plugins/nvim-cmp.lua
+++ .config/nvim/lua/user/plugins/nvim-cmp.lua
@@ -8,15 +8,37 @@ end
 
 local function make_snippet_behavior_table()
   local luasnip = require("luasnip")
-  return {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  }
+  return { expand = function(args) luasnip.lsp_expand(args.body) end }
 end
 
 local function make_mapping_table()
   local cmp = require("cmp")
+  local luasnip = require("luasnip")
+
+  local function make_tab_mapping()
+    return cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+  end
+
+  local function make_shift_tab_mapping()
+    return cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+  end
+
   return {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -24,7 +46,9 @@ local function make_mapping_table()
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = make_tab_mapping(),
+    ["<S-Tab>"] = make_shift_tab_mapping(),
   }
 end
 
