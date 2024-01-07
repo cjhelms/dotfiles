aa31f387f5c5da7adc3093ea1c7965f598580a6a
chris <cjhelms1428@gmail.com>
2024-01-06 22:40:26 -0600

    Move lualine to lazy

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -1,8 +1,13 @@
-require("lualine").setup({
-  options = {
+local function make_opts()
+  return {
     icons_enabled = false,
     theme = "gruvbox",
     component_separators = "|",
     section_separators = "",
-  },
-})
+  }
+end
+
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = make_opts()
+}
