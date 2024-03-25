d2e81e1328af3b0e17bdf028aeac80ccf19bdfeb
Chris Helms <cjhelms1428@gmail.com>
2024-03-25 00:11:57 -0500

    Update toggleterm open map

--- .config/nvim/lua/user/plugins/toggleterm.lua
+++ .config/nvim/lua/user/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      open_mapping = [[<leader>tt]],
+      open_mapping = [[<c-a>]],
       start_in_insert = false,
     })
   end,
