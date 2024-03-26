05e6036024d4eb67145a5ba677716558612ce705
Chris Helms <cjhelms1428@gmail.com>
2024-03-25 22:46:21 -0500

    Rewrite node install script

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,6 +1,3 @@
-sudo apt-get update && sudo apt-get install -y -q ca-certificates curl gnupg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo apt-key add -
-sudo chmod +r "/usr/share/keyrings/nodesource.gpg"
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update && sudo apt-get install -y -q nodejs
+sudo apt-get update && sudo apt-get install -y -q curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
