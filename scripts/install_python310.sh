b643a04840df0e1dd09f574ee826383b0b2c9fd2
Chris Helms <cjhelms1428@gmail.com>
2024-04-30 22:58:57 -0500

    Use apt-get instead of apt

--- scripts/install_python310.sh
+++ scripts/install_python310.sh
@@ -3,8 +3,8 @@ if [ -f /usr/bin/python3.10 ]; then
     echo "Python 3.10 installation found!"
     exit 0
 fi
-sudo apt update
-sudo apt install -y -q software-properties-common
+sudo apt-get update
+sudo apt-get install -y -q software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install -y -q python3.10 python3.10-venv python3.10-dev
+sudo apt-get update
+sudo apt-get install -y -q python3.10 python3.10-venv python3.10-dev
