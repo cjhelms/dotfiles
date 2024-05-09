f1f9afe14c0d06148dfd3b9373f05d5a173c345c
Chris Helms <cjhelms1428@gmail.com>
2024-05-08 22:29:13 -0500

    Bump lazygit

--- scripts/install_lazygit.sh
+++ scripts/install_lazygit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-VERSION="v0.40.2"
-TARBALL="lazygit_0.40.2_Linux_x86_64.tar.gz"
+VERSION="0.41.0"
+TARBALL="lazygit_${VERSION}_Linux_x86_64.tar.gz"
 mkdir -p ~/bin
 sudo apt-get update && sudo apt-get -yq install wget
 mkdir lazygit && cd lazygit
-wget "https://github.com/jesseduffield/lazygit/releases/download/${VERSION}/${TARBALL}"
+wget "https://github.com/jesseduffield/lazygit/releases/download/v${VERSION}/${TARBALL}"
 tar -xzvf "${TARBALL}"
 mv lazygit ~/bin
 cd .. && rm -rf lazygit
