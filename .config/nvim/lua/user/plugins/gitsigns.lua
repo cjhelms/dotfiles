1de6438ff81bf7a1403d460ac385e7bdeb24f73a
Chris Helms <cjhelms1428@gmail.com>
2024-11-23 23:24:00 -0600

    Add more gitsigns keybinds

--- .config/nvim/lua/user/plugins/gitsigns.lua
+++ .config/nvim/lua/user/plugins/gitsigns.lua
@@ -7,20 +7,20 @@ return {
       current_line_blame_formatter = "<author>, <abbrev_sha>, <author_time:%R> - <summary>",
     })
 
-    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_signs<cr>", {
-      silent = true,
-      desc = "[G]itsigns [T]oggle",
-    })
+    local function map(key, cmd, desc)
+      vim.api.nvim_set_keymap("n", "<leader>" .. key, ":Gitsigns " .. cmd .. "<cr>", {
+        silent = true,
+        desc = desc,
+      })
+    end
 
-    vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", {
-      silent = true,
-      desc = "[G]itsigns [N]ext hunk",
-    })
-
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<cr>", {
-      silent = true,
-      desc = "[G]itsigns [P]revious hunk",
-    })
+    map("gt", "toggle_signs", "[G]itsigns [T]oggle")
+    map("gn", "next_hunk", "[G]itsigns [N]ext hunk")
+    map("gp", "prev_hunk", "[G]itsigns [P]revious hunk")
+    map("gd", "toggle_deleted", "[G]itsigns toggle [D]eleted")
+    map("gs", "stage_hunk", "[G]itsigns [S]tage hunk")
+    map("gu", "undo_stage_hunk", "[G]itsigns [U]ndo stage hunk")
+    map("gr", "reset_hunk", "[G]itsigns [R]eset hunk")
 
     vim.keymap.set("n", "<leader>gb", function()
       require("gitsigns").blame_line({ full = true })
