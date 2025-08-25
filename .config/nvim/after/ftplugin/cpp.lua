99a9c128cbd3fc35dc4ef7f8d90a0c1d228fad8e
Chris Helms <cjhelms1428@gmail.com>
2025-08-25 08:38:27 -0500

    Add new nvim config

---
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,73 @@
+local function get_gtest_under_cursor()
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local node = ts_utils.get_node_at_cursor()
+  local bufnr = vim.api.nvim_get_current_buf()
+  if not node then
+    return nil
+  end
+  while node do
+    if node:type() == "function_definition" then
+      local declarator = node:child(0)
+      if declarator and declarator:type() == "function_declarator" then
+        local identifier = declarator:child(0)
+        if identifier and identifier:type() == "identifier" then
+          local name = vim.treesitter.get_node_text(identifier, bufnr)
+          if name == "TEST" or name == "TEST_F" or name == "TEST_P" then
+            local parameters = declarator:child(1)
+            if parameters and parameters:type() == "parameter_list" then
+              local text = vim.treesitter.get_node_text(parameters, bufnr)
+              local suite, test = text:match("([%w_]+)%s*,%s*([%w_]+)")
+              if suite and test then
+                return suite .. "." .. test
+              end
+            end
+          end
+        end
+      end
+    end
+    node = node:parent()
+  end
+  return nil
+end
+
+-- TODO: Detect if filter didn't match any tests.
+vim.api.nvim_create_user_command("GTestRun", function(opts)
+  if vim.g.gtest_executable == nil then
+    print("gtest_executable not set for this buffer")
+    return
+  end
+  local cmd = vim.g.gtest_executable .. " --gtest_filter=" .. opts.fargs[1]
+  vim.fn.setqflist({}, "r", { title = "GTest", lines = {}, efm = "%f:%l:%c: %m" })
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.fn.setqflist({}, "a", { lines = data })
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        vim.fn.setqflist({}, "a", { lines = data })
+      end
+    end,
+    on_exit = function(_, code)
+      if code ~= 0 and code ~= 1 then
+        print(vim.g.gtest_executable .. " failed with exit code: " .. code)
+        return
+      end
+      if code == 0 then
+        print(opts.fargs[1] .. " passed")
+        return
+      end
+      vim.cmd("copen")
+    end,
+  })
+end, {
+  nargs = 1,
+})
+
+vim.keymap.set("n", "<leader>tf", ":GTestRun ", { desc = "[T]est with [F]ilter" })
+
+vim.keymap.set("n", "<leader>tt", function()
+  vim.cmd("GTestRun " .. get_gtest_under_cursor())
+end, { desc = "[T]est under cursor" })
--- .config/nvim/lua/user/plugins/lazygit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kdheepak/lazygit.nvim",
-  config = function()
-    vim.keymap.set("n", "<leader>lg", ":LazyGit<cr>", {
-      silent = true,
-      desc = "[L]azy[G]it",
-    })
-  end,
-}
